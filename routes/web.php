9c5adc5c099cde64fcd3dd0b4f63c9d88639c50c
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-29 00:55:09 -0500

    Crean Controller con make y llamando la funcion en la ruta

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('principal');
 });
-Route::get('/crear-cuenta', function () {
-    return view('auth.register');
-});
+Route::get('/crear-cuenta', [RegisterController::class,'register'] );
