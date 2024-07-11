3c4053175811457d3fc60a05e52d07e227b46a97
Bryan Gallo <bryangallo95@hotmail.com>
2024-07-10 23:24:04 -0500

    Color de fondo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>SOCIAL SOCCER</title>
 </head>
 
-<body class="">
+<body class="bg-gray-100">
     <header class="p-5 border-b bg-white shadow">
         <div class="container mx-auto flex justify-between items-center">
             <h1 class="text-3xl font-black">Social Soccer</h1>
